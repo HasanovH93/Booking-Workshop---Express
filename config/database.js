4763ff0d9fd96e0ee2c4a6e0f1ef4f6efac0fddd
HasanovH93 <hasanovh14@gmail.com>
2022-09-28 18:29:04 +0200

    Create Service Updated

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ const connStr = "mongodb://localhost:27017/su-booking";
 
 module.exports = async (app) => {
     try {
-        await mongoose.connect(connStr,{
+        await mongoose.connect(connStr, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
-          });
-          console.log('DB connected')
+        });
+        console.log('DB connected')
     } catch (err) {
         console.error('Error initlializing database');
         console.error(err.message);
