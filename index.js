993fbf60b7d07843bed133c95653a16e7bd52116
HasanovH93 <hasanovh14@gmail.com>
2022-09-28 17:29:49 +0200

    otimized architecture

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 const express = require('express');
-const hbs = require('express-handlebars').create({
-    extname: '.hbs'
-});
+const expressConfig = require('./config/express')
+
 
 const homeController = require('./controllers/homeController');
 const catalogController = require('./controllers/catalogController');
 const createController = require('./controllers/createController');
 const defaultController = require('./controllers/defaultController');
-const defaultTitle = require('./middlewares/defaultTitle');
+
 
 const app = express();
 
-app.engine('.hbs', hbs.engine);
-app.set('view engine', '.hbs');
-
-app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static('static'));
-
-app.use(defaultTitle('SoftUni Accomodation'));
+expressConfig(app)
 
 app.use(homeController);
 app.use('/catalog', catalogController);
