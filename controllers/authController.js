88e39823ace60467aec09fa2a7b7ed85abe3c1ca
HasanovH93 <hasanovh14@gmail.com>
2022-10-05 20:10:18 +0200

    add login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,35 +1,20 @@
-const jwt = require('jsonwebtoken')
+const { login } = require('../services/authService')
 const authController  = require('express').Router()
 
-const jwtSecret = 'qpwkjadfdsfdsf';
 
-authController.get('/obtain', (req, res) => {
-    const payload = {
-        _id:"123010cc2023312efc",
-        username: 'Hasan',
-        roles: ['user']
+authController.get('/login', (req, res) => {
+     res.render('login', {
+        title: "Login"
+     })
+})
 
-    };
-    const token = jwt.sign(payload, jwtSecret, {expiresIn: '1h'});
+authController.post('/login', async (req, res) => {
+    const result = await login(req.body.username, req.body.password);
+    const token = req.signJwt(result)
     res.cookie('jwt', token)
-     res.send("Here is your token")
+    res.redirect('/')
 })
 
-authController.get('/validate', (req,res) => {
-    const token  = req.cookies.jwt;
-    if(token){
-        try {
-            const data = jwt.verify(token,jwtSecret)
-            console.log(data)
-            res.json(data)
-        } catch (err) {
-            res.cookie('jwt', '', {maxAge: 0});
-            res.redirect('/login')
-        }
-   
-    }else {
-        res.send('Missing token')
-    }
-})
+
 
 module.exports = authController;
---
+++ services/authService.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+
+async function login(username, password) {
+    console.log(username.toLowerCase())
+ return new Promise((res, rej) => {
+    if (username.toLowerCase() == "hasan" && password == "123456") {
+      res({
+        _id: "123010cc2023312efc",
+        username: "Hasan",
+        roles: ["user"],
+      });
+    }else {
+        rej(new Error('Incorrect username or password'))
+    }
+  });
+}
+
+module.exports = {
+  login,
+};
