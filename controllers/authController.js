55ef53f547b097496649f3bb4f000966c273571a
HasanovH93 <hasanovh14@gmail.com>
2022-10-05 18:22:52 +0200

    jwt config

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,16 @@
 const jwt = require('jsonwebtoken')
 const authController  = require('express').Router()
 
-const jwtSecret = 'qpwkjadfdsfdsf'
+const jwtSecret = 'qpwkjadfdsfdsf';
+
 authController.get('/obtain', (req, res) => {
     const payload = {
+        _id:"123010cc2023312efc",
         username: 'Hasan',
         roles: ['user']
 
     };
-    const token = jwt.sign(payload, jwtSecret);
+    const token = jwt.sign(payload, jwtSecret, {expiresIn: '1h'});
     res.cookie('jwt', token)
      res.send("Here is your token")
 })
@@ -16,9 +18,15 @@ authController.get('/obtain', (req, res) => {
 authController.get('/validate', (req,res) => {
     const token  = req.cookies.jwt;
     if(token){
-        const data = jwt.verify(token,jwtSecret)
-        console.log(data)
-        res.json(data)
+        try {
+            const data = jwt.verify(token,jwtSecret)
+            console.log(data)
+            res.json(data)
+        } catch (err) {
+            res.cookie('jwt', '', {maxAge: 0});
+            res.redirect('/login')
+        }
+   
     }else {
         res.send('Missing token')
     }
---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+
+
+
+module.exports = (jwtSecret) => (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    try {
+      const data = jwt.verify(token, jwtSecret);
+      req.user = data;
+    } catch (err) {
+      res.cookie("jwt", "", { maxAge: 0 });
+      return res.redirect("/login");
+    }
+  } 
+  next();
+};
