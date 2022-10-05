2544ead08c72c026b90600868abff76ba34f58b6
HasanovH93 <hasanovh14@gmail.com>
2022-10-05 17:39:20 +0200

    add auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,27 @@
+const jwt = require('jsonwebtoken')
 const authController  = require('express').Router()
 
+const jwtSecret = 'qpwkjadfdsfdsf'
 authController.get('/obtain', (req, res) => {
+    const payload = {
+        username: 'Hasan',
+        roles: ['user']
+
+    };
+    const token = jwt.sign(payload, jwtSecret);
+    res.cookie('jwt', token)
      res.send("Here is your token")
 })
 
 authController.get('/validate', (req,res) => {
-   res.send('Token validated')
+    const token  = req.cookies.jwt;
+    if(token){
+        const data = jwt.verify(token,jwtSecret)
+        console.log(data)
+        res.json(data)
+    }else {
+        res.send('Missing token')
+    }
 })
 
 module.exports = authController;
