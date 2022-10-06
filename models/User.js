5592d5bdfe97e149ab00c7727d95122cff2e81d4
HasanovH93 <hasanovh14@gmail.com>
2022-10-06 10:31:48 +0200

    add login func

--- models/User.js
+++ models/User.js
@@ -1,7 +1,17 @@
 const {Schema, model } = require('mongoose');
 
+
+const roleSchema = new Schema({
+    value: {type:String, enum: ['user', 'admin']}
+});
+
 const userSchema = new Schema({
     username: {type:String, required:true, minlength:3},
     hashedPassword: {type: String, required: true},
-    roles: {type:String , enum: ['user',['admin']]}
-})
+    roles: { type: [roleSchema], default: ['user'] }
+});
+
+
+const User = model('User',userSchema);
+
+module.exports = User;
