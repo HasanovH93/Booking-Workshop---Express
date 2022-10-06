e4ef08bf229b01dae2281f81387d5f4d7d99b82f
HasanovH93 <hasanovh14@gmail.com>
2022-10-06 13:03:22 +0200

    add owner field to model and service

--- middlewares/guards.js
+++ middlewares/guards.js
@@ -1,36 +1,34 @@
-function hasUser(){
-    return(req,res,next) => {
-        if(req.user != undefined){
-            next()
-        }else{
-            res.status(401).redirect('/')
-        }
+function hasUser() {
+  return (req, res, next) => {
+    if (req.user != undefined) {
+      next();
+    } else {
+      res.status(401).redirect("/auth/login");
     }
+  };
 }
 
-function isGuest(){
-    return(req,res,next) => {
-        if(req.user != undefined){
-            res.redirect('/auth/login')
-        }else{
-            next()
-        }
+function isGuest() {
+  return (req, res, next) => {
+    if (req.user != undefined) {
+      res.redirect("/");
+    } else {
+      next();
     }
+  };
 }
 
-function hasRole(role){
-    return(req, res, next) => {
-        if(req.user == undefined){
-            return res.status(401).redirect('/login')
-        }
-        if(req.user.roles.includes(role) == false) {
-            return res.status(403).redirect('/login')
-        }
-        next();
+function hasRole(role) {
+  return (req, res, next) => {
+    if (req.user == undefined || req.user.roles.includes(role) == false) {
+      res.redirect("/login");
+    } else {
+      next();
     }
+  };
 }
 
 module.exports = {
-    hasUser,
-    isGuest,
-}
+  hasUser,
+  isGuest,
+};
